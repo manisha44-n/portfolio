8d1251d6f3b813823346c7a2269a673e56fbeaec
student -newportfolio <manishajhinkwan@gmail.com>
2024-04-05 17:07:49 +0530

    update

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ window.onscroll = function() {
     myNav.classList.remove("scroll");
   }
 };
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click" ,()=>{
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n=>n.
+  addEventListener('click',()=>{
+    hamburger.classList.remove("active");
+    hamburger.classList.remove("active");
+  }))
